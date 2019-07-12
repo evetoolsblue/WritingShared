69ecbee050f92686493fd192df017d32544346fc
foxjazz <fox21@foxjazz.net>
2019-07-11 22:28:01 -0600

    first

---
+++ DoStuff.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FoxExtensions;
+using System.Threading;
+namespace WritingShared
+{
+    public static class DoStuff
+    {
+        public static string file;
+        public static string folder;
+        public static StreamWriter sw;
+        
+        public static FileStream fsr;
+        public static long InfoBytes;
+        public static long ReadAllBytes;
+        public static FileStream fsw;
+        public static bool tog;
+        public static void setup()
+        {
+            string root = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+
+            if (!Directory.Exists(root + "\\FileTest88"))
+                Directory.CreateDirectory(root + "\\FileTest88");
+            folder = root + "\\FileTest88";
+            file = folder + "\\test.txt";
+            if(File.Exists(file))
+                File.Delete(file);
+            var sr = File.CreateText(file);
+            sr.Close();
+            sr.Dispose();
+            using (var fsw = new FileStream(file, FileMode.Open, FileAccess.Write, FileShare.ReadWrite))
+            {
+                fsw.Write("test".ToBytes());
+            }
+            fsr = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            fsr.Close();
+            fsr.Dispose();
+            tog = true;
+
+        }
+        public static void readFileInfo()
+        {
+            var directory = new DirectoryInfo(folder);
+            var files = directory.GetFiles("test.txt");
+            InfoBytes = files[0].Length;
+
+        }
+
+        public static void ReadAllFile()
+        {
+            
+            using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            { 
+                ReadAllBytes = fs.Length;
+            }
+                
+        }
+        public static void WriteSomeBytes()
+        {
+            if (fsw == null) {
+                fsw = new FileStream(file, FileMode.Open, FileAccess.Write, FileShare.ReadWrite);
+            }
+            
+            
+                fsw.Write(" some".ToBytes());
+
+            if (tog)
+            {
+                fsw.Flush();
+                tog = false;
+            }
+            else
+            {
+                tog = true;
+            }
+        }
+
+        public static void StartTest()
+        {
+            bool stop = false;
+            while (!stop) {
+                if (Console.KeyAvailable)
+                {
+                    Console.WriteLine("key is available");
+                    if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+                    {
+
+                        stop = true;
+                    }
+                }
+
+                if (!stop)
+                {
+                    Thread.Sleep(500);
+                    WriteSomeBytes();
+                    readFileInfo();
+                    ReadAllFile();
+                    if(ReadAllBytes != InfoBytes)
+                    {
+                        Console.WriteLine($"readAllFile bytes: {ReadAllBytes} vs FileInfoBytes {InfoBytes}");
+                    }
+
+                }
+               }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WritingShared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting!");
+            DoStuff.setup();
+            WritingShared.DoStuff.StartTest();
+
+        }
+    }
+}
